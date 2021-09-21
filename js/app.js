9f8b1ec5e3157529e19008e4bd0621c326e98138
Virgile Reiter <virgile.reiter@gmail.com>
2021-09-21 20:30:59 +0200

    class creation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+
+
+document.getElementById('btn__reset').addEventListener('click', () =>{
+    game = new Game()
+    game.startGame()
+
+})
+
+document.getElementById('qwerty').addEventListener('click', (e) => {
+    if (e.target.tag === 'BUTTON'){
+        game.handleInteraction()
+    }
+
+})
